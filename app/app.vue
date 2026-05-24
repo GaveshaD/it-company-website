808edd7f4f9196d637095e41c8bf51461f4f7b16
GaveshaD <kavindugavesha0@gmail.com>
2026-05-24 16:30:13 +0530

    Added navigation bar and styling

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,37 @@
 <template>
-  <NuxtPage />
+  <div>
+    <nav class="navbar">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/services">Services</NuxtLink>
+    </nav>
+
+    <div class="container">
+    <NuxtPage />
+  </div>
+</div>
 </template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+}
+
+.navbar {
+  background-color: #222;
+  padding: 20px;
+  display: flex;
+  gap: 20px;
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.container {
+  padding: 30px;
+}
+</style>
