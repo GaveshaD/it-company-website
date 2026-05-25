d87409b73449511ec4f0e3ab645848e2dec1f60d
GaveshaD <kavindugavesha0@gmail.com>
2026-05-25 22:14:42 +0530

    Create projects page

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,8 @@
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/about">About</NuxtLink>
       <NuxtLink to="/services">Services</NuxtLink>
+      <NuxtLink to="/projects">Projects</NuxtLink>
+      <NuxtLink to="/contact">Contact</NuxtLink>
     </nav>
 
     <div class="container">
