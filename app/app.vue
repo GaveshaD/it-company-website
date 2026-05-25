b409be9519b1428dd8d5b600986ea16f3a4b8c1f
GaveshaD <kavindugavesha0@gmail.com>
2026-05-25 22:16:52 +0530

    Remove contact bar

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@
       <NuxtLink to="/about">About</NuxtLink>
       <NuxtLink to="/services">Services</NuxtLink>
       <NuxtLink to="/projects">Projects</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
+      
     </nav>
 
     <div class="container">
