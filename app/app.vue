284861246dd43a85903f0028b7cb1c0aeba6b998
GaveshaD <kavindugavesha0@gmail.com>
2026-05-27 01:17:57 +0530

    Create reusable navbar component

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,15 @@
 <template>
   <div>
-   <nav class="navbar">
-  <NuxtLink to="/">Home</NuxtLink>
-  <NuxtLink to="/about">About</NuxtLink>
-  <NuxtLink to="/services">Services</NuxtLink>
-  <NuxtLink to="/contact">Contact</NuxtLink>
-  <NuxtLink to="/projects">Projects</NuxtLink>
-</nav>
+    
+  <Navbar />
+  
+
 
     <div class="container">
     <NuxtPage />
   </div>
+  
+ <FooterSection />
 </div>
 </template>
 
@@ -20,24 +19,7 @@ body {
   margin: 0;
 }
 
-.navbar {
-  background-color: #222;
-  padding: 20px;
-  display: flex;
-  gap: 20px;
-}
 
-.navbar a {
-  color: white;
-  text-decoration: none;
-  font-weight: bold;
-}
-
-.router-link-exact-active {
-  color: #00aaff !important;
-  border-bottom: 2px solid #00aaff;
-  padding-bottom: 5px;
-}
 
 .container {
   padding: 30px;
---
+++ app/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar">
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/about">About</NuxtLink>
+    <NuxtLink to="/services">Services</NuxtLink>
+    <NuxtLink to="/projects">Projects</NuxtLink>
+    <NuxtLink to="/contact">Contact</NuxtLink>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  background-color: #222;
+  padding: 20px;
+  display: flex;
+  gap: 20px;
+}
+
+.navbar a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+.router-link-exact-active {
+  color: #00aaff !important;
+  border-bottom: 2px solid #00aaff;
+  padding-bottom: 5px;
+}
+</style>
