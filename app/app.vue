a3ed863dc3f2d41a18b4d558f6a14452b49660d3
GaveshaD <kavindugavesha0@gmail.com>
2026-05-30 19:31:15 +0530

    Add dark mode toggle

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,23 @@
 <template>
-  <div>
-    
-  <Navbar />
+  <div :class="{ dark: isDark }">
+    <Navbar />
+
+    <button @click="isDark = !isDark">
+      {{ isDark ? "☀️ Light Mode" : "🌙 Dark Mode" }}
+    </button>
 
     <div class="container">
-    <NuxtPage />
+      <NuxtPage />
+    </div>
+
+    <FooterSection />
   </div>
-  
- <FooterSection />
-</div>
 </template>
+
+<script setup>
+  const isDark = ref(false)
+</script>
+
 
 <style>
 body {
@@ -19,4 +27,18 @@ body {
 .container {
   padding: 30px;
 }
+.dark {
+  background: #121212;
+  color: white;
+  min-height: 100vh;
+}
+
+.dark input,
+.dark textarea {
+  background: #222;
+  color: white;
+  border: 1px solid #444;
+}
 </style>
+
+
