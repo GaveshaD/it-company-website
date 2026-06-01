4886ce332f3fae490978262368105196de471f7b
GaveshaD <kavindugavesha0@gmail.com>
2026-06-01 19:22:08 +0530

    Display project category on cards

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -39,6 +39,7 @@
         :key="project.title"
         :title="project.title"
         :description="project.description"
+        :category="project.category"
         
       />
       <p v-if="filteredProjects.length === 0" class="no-projects">
